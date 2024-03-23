34d569bdb3f67ab71752d0fcaaccc908da74e298
Kashif Ali1 <160309336+kashifali34@users.noreply.github.com>
2024-03-23 01:06:49 -0700

    created number gussing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGussedNumber",
+        type: "number",
+        message: "Please Guess A number between 1-6"
+    },
+]);
+if (answer.userGussedNumber === 13) {
+    console.log("congratulation! you guessed right number");
+}
+else {
+    console.log("you gussed wrong number");
+}
